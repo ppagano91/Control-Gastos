2040042aad4287df03c12ab13b87484d1b2f9bad
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-13 14:18:48 -0300

    Validando Presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+import Mensaje from './Mensaje';
 
 const NuevoPresupuesto = ({presupuesto,setPresupuesto}) => {
+  const [mensaje, setMensaje] = useState("")
+
+  const handlePresupuesto = (e) =>{
+    e.preventDefault();
+
+    if (!Number(presupuesto) || Number(presupuesto) < 0){
+      setMensaje("No es un presupuesto válido")            
+    }else{
+      
+    }
+
+
+  }
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form className='formulario' onSubmit={handlePresupuesto}>
             <div className='campo'>
                 <label htmlFor="">Definir Presupuesto</label>
                 <input 
@@ -16,6 +31,7 @@ const NuevoPresupuesto = ({presupuesto,setPresupuesto}) => {
             </div>
 
             <input type="submit" value="Añadir"/>
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         
         </form>
     </div>
