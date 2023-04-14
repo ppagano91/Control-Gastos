50f207f1204f0c87fc4b59c0d1e3181a72352558
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-14 09:09:44 -0300

    Mostrando Gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Gasto = () => {
+const Gasto = ({gasto}) => {
+    const {categoria, nombre, cantidad, id} = gasto;
   return (
-    <div>Gasto</div>
+    <div className='gasto sombra'>
+        <div className='contenido-gasto'>
+            <div className='descripcion-gasto'>
+                <p className='categoria'>
+                    {categoria}
+                </p>
+                <p className='categoria'>
+                    {nombre}
+                </p>
+                <p className='nombre-gasto'>
+                    {cantidad}
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
